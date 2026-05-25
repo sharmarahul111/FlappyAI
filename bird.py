ace0895ff558fd69e730f5e0462ca55cdf1170e3
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-25 17:15:19 +0545

    bird class with draw and update method and key control

---
+++ bird.py
@@ -0,0 +1,51 @@
+from settings import *
+from pyray import *
+class Bird:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+		self.radius = 20
+		self.color = YELLOW
+
+		self.velocity_y = 0
+
+		# gravity settings
+		self.gravity = 0.5
+		self.max_fall_speed = 12
+
+	def update(self):
+		# flap
+		if is_key_pressed(KEY_SPACE):
+			self.velocity_y = -8
+
+		# gravity
+		self.velocity_y += self.gravity
+
+		if self.velocity_y > self.max_fall_speed:
+			self.velocity_y = self.max_fall_speed
+
+		# move
+		self.y += self.velocity_y
+
+		# top collision
+		ceiling_y = self.radius
+
+		if self.y < ceiling_y:
+			self.y = ceiling_y
+			self.velocity_y = 0
+
+		# bottom collision
+		floor_y = WINDOW_HEIGHT - self.radius
+
+		if self.y > floor_y:
+			self.y = floor_y
+			self.velocity_y = 0
+
+	def draw(self):
+		draw_circle(
+			int(self.x),
+			int(self.y),
+			self.radius,
+			self.color
+		)
